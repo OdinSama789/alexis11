1ba723c8b18ac940db140e2b78d516a45356c6e0
ODIN_SAMA <202213854k@utea.edu.pe>
2024-03-04 11:01:49 -0500

    aea

---
+++ Ejercicios07/ejercicio1.java
@@ -0,0 +1,29 @@
+package Ejercicios07;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        int[] num = new int[12];
+        double[] arreglo; 
+        for (int i = 0; i < num.length; i++) {
+            num[i] = 12;
+    }
+    num =  new int[11];
+    arreglo = new double[12];
+
+    num[0] = 39;
+    num[1] = -2;
+    num[2] = ;
+    num[3] = ;
+    num[4] = 0;
+    num[5] = ;
+    num[6] = 14;
+    num[7] = ; 
+    num[8] =; 
+    num[9] = 120; 
+    num[10] = ; 
+    num[11] = ; 
+    num[12] = ;
+
+    System.out.println(num[0]);
+}
+}
