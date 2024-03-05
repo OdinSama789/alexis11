474b5fab9001db419f999be0cbac4f616e2c37fc
ODIN_SAMA <202213854k@utea.edu.pe>
2024-03-05 12:09:39 -0500

    Array042

---
+++ Array042.java
@@ -0,0 +1,17 @@
+public class Array042 {
+    public static void main(String[] args) {
+        /*
+         * Escribe un programa que llene un array de 100 alementos con numeros aleatorios entre 0 y 500
+         * A continuacion el programa mostrara el array y preguntara al usuario si quiere destacar el maximo
+         * o minimo, seguidamene se volvera a mostrar el array escribiendo el numero destacdo entre dobles asteriscos
+         * 
+         * **1**
+         * **480**
+         */
+        int[] numeros = new int[100];
+        
+        for (int i = 0; i < numeros.length; i++) {
+        int aleatorio = (int)(Math.random() *501)+1;
+        }
+    }
+}
