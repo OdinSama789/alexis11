c467ef35d5f1eee8a0567fe27eb418f7986bfd3b
ODIN_SAMA <202213854k@utea.edu.pe>
2024-03-12 11:14:14 -0500

    bidimencional

--- ArrayBi01.java
+++ ArrayBi01.java
@@ -1,3 +1,20 @@
 public class ArrayBi01 {
-    
+    público  estático  vacío  principal ( String [] args ) {
+        int [][] n = nuevo  int [ 3 ][ 2 ];
+        int  fila , columna ;
+        //Llenar el arreglo 3x2
+        para ( fila = 0 ; fila < 3 ; fila ++) {
+            para ( columna = 0 ; columna < 2 ; columna ++) {
+                n [ fila ][ columna ] = ( int ) ( Math . random () * 10 );
+            }
+        }
+        //Mostrar el arreglo
+        para ( fila = 0 ; fila < 3 ; fila ++) {
+            Sistema . afuera . imprimir ( "Fila: " + fila );
+            para ( columna = 0 ; columna < 2 ; columna ++) {
+                Sistema . afuera . printf ( "%10d " , n [ fila ][ columna ]);
+            }
+            Sistema . afuera . imprimir ();
+        }
+    }
 }
